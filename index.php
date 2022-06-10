c79e80c4ed19f6c72749fae54ab0dfc61608263d
jean <jeancris95@gmail.com>
2022-06-10 21:06:53 +0200

    prueba completa para subir al servidor

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>CAMPUS</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
 
   <!-- Favicons -->
   <link href="assets/img/CAMPUS.png" rel="icon">
